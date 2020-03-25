6723817505b5f9f26a59b4e3a622b6101d7ef74c
JacobMarshall0 <60146698+JacobMarshall0@users.noreply.github.com>
2020-03-25 15:23:15 +0000

    Add files via upload

---
+++ OOP Assignment 2/OOP Assignment 2/Git.cs
@@ -0,0 +1,21 @@
+﻿// the git class ideally would hold git functions
+using System.Collections.Generic;
+public class Git
+{
+	public bool diff(string[] file1, string[] file2) // git diff takes two files and indicates the differences between
+	{
+		if (file1.Length == file2.Length) // if the length is the same
+		{
+			for (int i = 0; i < file1.Length; i++) // cycles through the files
+			{
+				if (file1[i] != file2[i]) // compares values at the same index
+				{
+					return false; // if it is not the same it returns false
+				}
+			}
+			return true; // this means that all the values are the same and therefore the files are identical
+		}
+		return false; // if the length is different the files cannot be identical
+	}
+}
+
---
+++ OOP Assignment 2/OOP Assignment 2/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+class Program
+{
+    static string GetFile() // function to get the location of the file
+    {
+        Console.WriteLine("Please enter the location of the file:");
+        string file = Console.ReadLine(); // assigns user input to string
+        return file; // returns string
+    }
+    static void Main()
+    {
+        // Explains what the program will do
+        Console.WriteLine("Git diff compares files and returns the differences between them");
+
+        string file1 = GetFile();
+        string file2 = GetFile();
+
+        while (File.Exists(file1) == false)
+        {
+            Console.WriteLine("File 1 does not exist, remember .txt and check the location you entered", Console.ForegroundColor = ConsoleColor.Yellow);
+            file1 = GetFile();
+        }
+        
+        while (File.Exists(file2) == false)
+        {
+            Console.WriteLine("File 2 does not exist, remember .txt and check the location you entered", Console.ForegroundColor = ConsoleColor.Yellow);
+            file2 = GetFile();
+        }
+
+        //reads file name or location into memory
+        string[] file1Array = { }; //instantiates array of strings for each line from file
+        string[] file2Array = { };
+        
+        file1Array = File.ReadAllLines(file1); //reads lines into array
+        file2Array = File.ReadAllLines(file2);
+        
+        Git git = new Git(); //creates instance of Git class
+        bool result = git.diff(file1Array,file2Array); // creates variable result and assigns it with the boolean value from the diff function
+        if (result == true) // if the files are identical
+        {
+            Console.WriteLine("These files are identical", Console.ForegroundColor = ConsoleColor.Green); // prints message in green, indicating the same
+        }
+        else // if the files are not identical
+        {
+            Console.WriteLine("These files are not the same", Console.ForegroundColor = ConsoleColor.Red); // prints message in red, indicating that these are not the same
+        }
+        Console.ForegroundColor = ConsoleColor.Black; // hides console closing text
+
+    }
+}
